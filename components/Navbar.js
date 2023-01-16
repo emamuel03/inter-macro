ce6d2a7524e735a7aa7dcbc90493b093b0d0ee9a
Emmanuel Gómez <emmanuel.gomez.03@gmail.com>
2023-01-16 11:51:35 -0300

    cerrar sesion

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import styles from '../styles/Components.module.css'
 import AuthContext from '../context/AuthContext'
 import { useAuthUser } from '../hooks/useAuthUser'
+import { signOut } from 'firebase/auth'
+import { auth } from "../firebase";
 
 function Navbar() {
     useAuthUser();
     const { isLogged } = useContext(AuthContext);
+    const [current, setCurrent] = useState();
+
+    const logOut = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+      auth.onAuthStateChanged((u)=>{
+        setCurrent(u)
+      })
+    }, [])  
+    
     return (
         <nav className={styles.nav_main}>
             <div className={styles.container_fluid}>
@@ -17,6 +35,14 @@ function Navbar() {
                     <div className={styles.nav_item}>
                         <Link href="/login"><h5>Iniciar Sesión</h5></Link>
                     </div>
+                )}
+                 {isLogged === true && (
+                    <>
+                    <div className={styles.nav_item}>
+                        <Link href="/login"><h5>{current.email}</h5></Link>
+                    </div>
+                    <button onClick={logOut}>Cerrar Sesión</button>
+                    </>
                 )}
             </div>
         </nav>
